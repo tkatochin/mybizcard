93e289f241ba3cae4efcf2f146020fa8bc57e8b2
加藤 <kato@gluegent.com>
2018-10-03 03:52:25 +0900

    card scale up on focus and links lighting on hover

--- bizcard.js
+++ bizcard.js
@@ -2,7 +2,15 @@ $(window).on("load", function(){
     if(navigator.userAgent.match(/(iPhone|iPad|iPod|Android)/)){
         $(".area-facebook").attr("href", "fb://profile/100001313416898");
     }
-    $("a").attr("target", "_blank").on("click", function(evt){
+    $("a").attr("target", "_blank")
+    /*.on("click", function(evt){
         evt.stopPropagation();
     })
+    */
+    $(".flip-container").on("click", function(ev){
+        if ($(ev.target).hasClass("front")
+            || $(ev.target).hasClass("back")) {
+            this.classList.toggle('toggle');
+        }
+    });
 });
